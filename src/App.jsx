3cc64d01996dd44dd417f63941177e1b12ebe260
Laiba Afzal <laibaafzal042@gmail.com>
2026-04-21 07:51:44 -0700

    final push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-
-import Header from './Components/Header'
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Products from './Pages/Products';
 import Contact from './Pages/Contact';
-import Footer from './Components/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
